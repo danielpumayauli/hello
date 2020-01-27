f531573523c7b7c3db1760b27ab40e70d667f584
Daniel Pumayauli <danielpumayauli@gmail.com>
2020-01-27 18:15:50 -0500

    Updates

---
+++ precache-manifest.f92fbda2de73323ce20e9b241d66e932.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59d87342112556ada51903492a4eb56",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "7e8f1bdc7496eeefdad5",
+    "url": "/hello/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "95b661d4d4129b38619a",
+    "url": "/hello/static/js/2.6949f162.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hello/static/js/2.6949f162.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e8f1bdc7496eeefdad5",
+    "url": "/hello/static/js/main.060c1434.chunk.js"
+  },
+  {
+    "revision": "de368dcbb9a951cc3091",
+    "url": "/hello/static/js/runtime-main.033cf731.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
